1d4f13b091b1a9c56fe724c4ee12dd1b9bce5042
jd119592 <jed.fuller@my.jcu.edu.au>
2026-06-12 23:42:33 +1000

    Refactor process function in score program

--- prac_02/Score.py
+++ prac_02/Score.py
@@ -2,13 +2,24 @@
 CP1404/CP5632 - Practical
 Program to determine score status
 """
+# from unittest import result
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+
+def main():
+    score = float(input("Enter score: "))
+    result = process_score(score)
+    print(f"Your results are!: {result}")
+
+def process_score(score):
+    if score < 0 or score > 100:
+        result = "Invalid score"
+    elif score >= 90:
+        result = "Excellent"
+    elif score >= 50:
+        result = "Passable"
+    else:
+        result = "Bad"
+    return result
+
+main()
+
