0531457b6e9b071a3d5760b8964f249cb9644ae7
jd119592 <jed.fuller@my.jcu.edu.au>
2026-06-17 00:53:38 +1000

    Add subject reader polished, gives information about subject list. After refactoring and indexing

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -9,11 +9,16 @@ FILENAME = "subject_data.txt"
 def main():
     """Program to load and display subject data from file."""
     data = load_data(FILENAME)
-    print(data)
 
+    print_subj_details(data)
+
+def print_subj_details(data):
+    for i in range(0,len(data)):
+        print(f"{data[i][0]} is taught by {data[i][1]} and has {data[i][2]} students")
 
 def load_data(filename=FILENAME):
     """Read data from file formatted like: subject,lecturer,number of students."""
+    data_nest = []
     input_file = open(filename)
     for line in input_file:
         print(line)  # See what a line looks like
@@ -23,9 +28,11 @@ def load_data(filename=FILENAME):
         print(parts)  # See what the parts look like (notice the integer is a string)
         # Make the number an integer as part of a new, poorly named, list
         data = [parts[0], parts[1], int(parts[2])]
-        print(data)  # See if that worked
+        data_nest.append(data)
+        print(data) # See if that worked
         print("----------")
     input_file.close()
-
+    print(data_nest)
+    return data_nest
 
 main()
