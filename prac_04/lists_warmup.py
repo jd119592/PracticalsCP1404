e41643002eed04df2671831439149c7861c8c3d0
jd119592 <jed.fuller@my.jcu.edu.au>
2026-06-16 23:38:53 +1000

    Add list indexing demonstration

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,21 @@
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+# numbers[0] = 3
+# numbers[-1] = 2
+# numbers[3] = 1
+# numbers[:-1] = [3, 1, 4, 1, 5, 9]
+# numbers[3:4] = [1]
+# 5 in numbers = True
+# 7 in numbers = False
+# "3" in numbers =  False
+# numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+numbers[0] = "ten"
+numbers[5] = 1
+if 9 in numbers:
+    print("9 is in numbers")
+else:
+    print("9 is not in numbers")
+print(numbers[2:7])
+
+
