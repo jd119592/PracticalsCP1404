0878c5bbbcf04293f8ad8d6244402aa226655340
jd119592 <jed.fuller@my.jcu.edu.au>
2026-06-16 23:56:25 +1000

    Add total income counter refactoring demonstration

---
+++ prac_04/total_income.py
@@ -0,0 +1,29 @@
+"""
+CP1404/CP5632 Practical
+Starter code for cumulative total income program
+"""
+from typing import Any
+
+
+def main():
+    """Display income report for incomes over a given number of months."""
+    incomes = []
+    number_of_months = int(input("How many months? "))
+
+    for month in range(1, number_of_months + 1):
+        income = float(input(f"Enter income for month {month} ($):"))
+        incomes.append(income)
+    print_report(incomes, number_of_months)
+
+
+def print_report(incomes: list[Any], number_of_months: int):
+    print("\nIncome Report\n-------------")
+    total = 0
+    for month in range(1, number_of_months + 1):
+        income = incomes[month - 1]
+        total += income
+        # print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        print(f"Month:{month}   Income:${income:.2f}  Total:${total:.2f}")
+
+
+main()
