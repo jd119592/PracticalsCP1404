9e20ec5b73db98151d41d8cddf43a17613e1ca7a
jd119592 <jed.fuller@my.jcu.edu.au>
2026-06-18 16:00:38 +1000

    Add json test to demonstrate how to use json to access data in a function

---
+++ prac_05/jsontest.py
@@ -0,0 +1,16 @@
+# import json
+#
+# demo_text = """{"number": 1, "title": "Demo", "state": "open"}"""
+
+# URL =  "Website url to process data"
+#
+# thing = json.loads(demo_text)
+# print(thing)
+# print(type(thing))
+# print(thing['title'])
+# thing['title'] = thing['title'].upper()
+# print(thing['title'])
+#
+# text = json.dumps(thing)
+# print(text)
+# print(type(text))
