61093fcfeca847ec78549425d5a5860bb7f48e66
jd119592 <jed.fuller@my.jcu.edu.au>
2026-06-23 21:23:56 +1000

    update correct names for state names file

--- prac_05/State_names.py
+++ prac_05/State_names.py
@@ -7,25 +7,25 @@ CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern T
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
 print(CODE_TO_NAME)
 
-state_code = input("Enter short state: ")
-while state_code != "":
-    # try state_codevislower():
+short_state = input("Enter short state: ")
+while short_state != "":
+    # try short_statevislower():
     for i in CODE_TO_NAME:
         print(f"{i:4} is {CODE_TO_NAME[i]:4}")
 
-    state_code = state_code.upper()
+    short_state = short_state.upper()
 
-    # if state_code in CODE_TO_NAME:
-    #     print("your pick:",state_code, "is", CODE_TO_NAME[state_code])
+    # if short_state in CODE_TO_NAME:
+    #     print("your pick:",short_state, "is", CODE_TO_NAME[short_state])
     # else:
     #     print("Invalid short state")
 
-    # for state_code in CODE_TO_NAME:
+    # for short_state in CODE_TO_NAME:
     try:
-        print("your pick:", state_code, "is", CODE_TO_NAME[state_code])
+        print("your pick:", short_state, "is", CODE_TO_NAME[short_state])
     except KeyError:
         print("Invalid short state")
 
 
 
-    state_code = input("Enter short state: ")
+    short_state = input("Enter short state: ")
