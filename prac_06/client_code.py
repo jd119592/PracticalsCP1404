4b802a3c43976b1fd58b56b6bbeee8e5e6069085
jd119592 <jed.fuller@my.jcu.edu.au>
2026-07-30 16:24:40 +1000

    Add wk6seminar and client_code to demonstrate classes and calling classes across files

--- prac_06/client_code.py
+++ prac_06/client_code.py
@@ -1,5 +1,5 @@
 
-from prac_06.wk6seminar import Product
+from prac_06.wk6seminarCLASSES import Product
 
 products = [Product("Phone", 340, False), Product("PC",
  1420.95, True), Product("Plant", 24.5, True)]
